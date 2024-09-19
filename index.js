106eba4c45be81cf863557caf0170f59d7faf21c
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-09-19 21:43:04 +0300

    User, Product models

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-
+mongoose.connect("mongodb+srv://mo:momo@cluster0.swqyn.mongodb.net/")
+.then(()=> console.log("db connected successfully"))
+.catch((err) => {console.log(err)})
 app.listen(5000, ()=> {
   console.log("Server is running");
 })
