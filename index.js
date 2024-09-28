bfe5c540aa65415a8822a10e004d927bd38243de
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-09-28 18:13:50 +0300

    login routes

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const authRoute = require("./routes/auth")
 dotenv.config()
 
 mongoose.connect(process.env.Mongo_URL)
 .then(()=> console.log("db connected successfully"))
 .catch((err) => {console.log(err)})
 
-app.listen(5000, ()=> {
-  console.log("Server is running");
-})
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-app.r
+app.use("/api/auth", authRoute)
+
+
+app.listen(5000, ()=> {
+  console.log("Server is running");
+})
+
